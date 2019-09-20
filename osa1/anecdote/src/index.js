f303a24c64b18411172b4be84e63aea8afa10fac
Renny Lindberg <reizka.deepone@gmail.com>
2019-09-20 20:04:06 +0200

    anecdote

--- osa1/anecdote/src/index.js
+++ osa1/anecdote/src/index.js
@@ -1,16 +1,78 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const ShowVote =(props) =>{
+    let val = points[props.votes]
+    console.log('Show vote');
+    return(
+        <div>
+            <p>Votes given {val}</p>
+        </div>
+    )
+}
+
+const MostPopularAnecdote =()=>{
+    console.log('MostPopularAnecdote');
+    let highest = 0;
+    let c = 0; 
+    points.forEach(element => {
+        if(c>0){
+            if(element>points[c-1]){
+                highest = c;
+            }
+        } 
+        c++ 
+    });
+
+    console.log('SelectedIn', highest);
+
+    return(
+        <div>
+            <p>Most popular anecdote is:</p>
+            <p>{anecdotes[highest]}</p>
+            <p>With {points[highest]} votes</p>
+        </div>
+    )
+}
+
+const Button = (props) => (
+    <button onClick={props.onClick}>
+      {props.text}
+     
+    </button>
+
+    
+  )
+
+
 const App = (props) => {
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [votes, setVote] = useState(0);
+
+
+ const selectRandom =()=>{
+      let val = Math.floor(Math.random() * anecdotes.length);
+      setSelected(val) 
+  }
+
+  const vote =()=>{
+      let v = points[selected]+1
+      setVote(v) //only way I could get the view update...dropped the ball at somepoint I supposed
+      points[selected] =v;
+     
+  }
 
   return (
     <div>
-      {props.anecdotes[selected]}
+     <p>{props.anecdotes[selected]}</p>
+      <Button onClick={selectRandom} text={'Random anecdote'}/>
+      <Button onClick={vote} text={"Vote"}/>
+      <ShowVote votes={selected}/>
+      <MostPopularAnecdote/>
     </div>
   )
 }
-
+//<p>Votes given {points.find(selected)}</p>
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -19,7 +81,7 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
-
+const points = Array(anecdotes.length).fill(0);
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
   document.getElementById('root')
