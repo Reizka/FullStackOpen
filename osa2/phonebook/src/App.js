9cc92fb2cfe702f81d053fa34b0e82ae15ef4ebe
Renny Lindberg <reizka.deepone@gmail.com>
2019-09-23 20:38:40 +0200

    name & number

--- osa2/phonebook/src/App.js
+++ osa2/phonebook/src/App.js
@@ -1,41 +1,57 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "0123456789" }
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState(0);
 
   const addPerson = function(event) {
     event.preventDefault();
     console.log("button clicked", event.target);
-    const personObject = { name: newName };
-    //date: new Date().toISOString(),
-    //important: Math.random() > 0.5,
-    //id: notes.length + 1,
+    console.log("list", persons);
+    console.log("included");
+    if (persons.some(person => person.name === newName) || newName === "") {
+      alert(`${newName} already in the list or field is empty`);
+    } else {
+      const personObject = { name: newName, number: newNumber };
+      setPersons(persons.concat(personObject));
+    }
 
-    setPersons(persons.concat(personObject));
     setNewName("");
+    setNewNumber("");
   };
 
-  const handleDataChange = event => {
+  const handleNewNameField = event => {
     //console.log(event.target.value);
     setNewName(event.target.value);
+  };
+
+  const handleNewNumberField = event => {
+    //console.log(event.target.value);
+    setNewNumber(event.target.value);
   };
 
   const pm = persons.map(function(person, i) {
     return (
       <>
-        <li key={i}>Name: {person.name}</li>
+        <li key={i}>
+          Name: {person.name} number: {person.number}
+        </li>
       </>
     );
   });
 
-  console.log("list", persons);
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleDataChange} />
+          name: <input value={newName} onChange={handleNewNameField} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNewNumberField} />
         </div>
         <div>
           <button type="submit">add</button>
