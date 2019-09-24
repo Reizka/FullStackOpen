41231be1f288688100c9c0c9f24725a96f2dc8a1
Renny Lindberg <reizka.deepone@gmail.com>
2019-09-24 21:24:28 +0200

    filter added

--- osa2/phonebook/src/App.js
+++ osa2/phonebook/src/App.js
@@ -1,17 +1,92 @@
 import React, { useState } from "react";
 
+const Filter = function({ newFilter, setFilter }) {
+  const handleFilter = event => {
+    console.log(event.target.value);
+    setFilter(event.target.value);
+  };
+
+  return (
+    <form>
+      <div>
+        Filter: <input value={newFilter} onChange={handleFilter}></input>
+      </div>
+    </form>
+  );
+};
+
+const PersonForm = function({
+  addPerson,
+  newName,
+  newNumber,
+  setNewName,
+  setNewNumber
+}) {
+  const handleNewNameField = event => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+  const handleNewNumberField = event => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
+  return (
+    <form onSubmit={() => addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNewNameField} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNewNumberField} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = function({ newFilter, persons }) {
+  let filtPersons;
+  console.log("newFilter", newFilter);
+  if (newFilter !== "") {
+    filtPersons = persons.filter(function(person) {
+      const lowCase = newFilter.toLowerCase();
+      console.log("filtered", lowCase);
+      return person.name.toLowerCase() === lowCase;
+    });
+  } else {
+    console.log("not filtered");
+    filtPersons = persons;
+  }
+  console.log("filtered", filtPersons);
+  const pm = filtPersons.map(function(person, i) {
+    return (
+      <>
+        <li key={i}>
+          Name: {person.name} number: {person.number}
+        </li>
+      </>
+    );
+  });
+
+  return <ul>{pm}</ul>;
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "0123456789" }
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
   ]);
   const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState(0);
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setFilter] = useState("");
 
   const addPerson = function(event) {
     event.preventDefault();
-    console.log("button clicked", event.target);
-    console.log("list", persons);
-    console.log("included");
+
     if (persons.some(person => person.name === newName) || newName === "") {
       alert(`${newName} already in the list or field is empty`);
     } else {
@@ -23,44 +98,24 @@ const App = () => {
     setNewNumber("");
   };
 
-  const handleNewNameField = event => {
-    //console.log(event.target.value);
-    setNewName(event.target.value);
-  };
-
-  const handleNewNumberField = event => {
-    //console.log(event.target.value);
-    setNewNumber(event.target.value);
-  };
-
-  const pm = persons.map(function(person, i) {
-    return (
-      <>
-        <li key={i}>
-          Name: {person.name} number: {person.number}
-        </li>
-      </>
-    );
-  });
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNewNameField} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNewNumberField} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>{pm}</ul>
+      <Filter newFilter={newFilter} setFilter={setFilter} />
+
+      <h3>Add a new</h3>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        newNumber={newNumber}
+        setNewName={setNewName}
+        setNewNumber={setNewNumber}
+      />
+
+      <h3>Numbers</h3>
+      <Persons persons={persons} newFilter={newFilter} />
     </div>
   );
 };
-
+//<Persons newFilter={newFilter} persons={persons} />
 export default App;
