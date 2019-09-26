06089c526f443f03b948ddb7a3a9a942a60f36a8
Renny Lindberg <reizka.deepone@gmail.com>
2019-09-26 10:40:45 +0200

    weather api

--- osa2/countries/src/App.js
+++ osa2/countries/src/App.js
@@ -1,6 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const api = "d573b08a0d3c269cca0533a25bd3333b";
+
+const GetWeather = function({ weather, capital }) {
+  console.log("weather", weather);
+  if (weather.current) {
+    return (
+      <>
+        <h2>weather in {capital}</h2>
+        <p>
+          <b>Temperature</b>
+          {weather.current.temperature}
+        </p>
+        <img width="100px" src={weather.current.weather_icons} alt="flag" />
+        <p>
+          <b>Wind</b>
+          {weather.current.wind_speed}
+        </p>
+      </>
+    );
+  } else {
+    return <></>;
+  }
+};
+
 const FilterForm = function({ setFilter, newFilter }) {
   const handleFilterField = event => {
     console.log(event.target.value);
@@ -17,7 +41,12 @@ const FilterForm = function({ setFilter, newFilter }) {
   );
 };
 
-const CountryFiltered = function({ countries, newFilter }) {
+const CountryFiltered = function({
+  countries,
+  newFilter,
+  setFilter,
+  setCapital
+}) {
   console.log("countries", countries);
 
   if (countries.length === 0) {
@@ -31,6 +60,11 @@ const CountryFiltered = function({ countries, newFilter }) {
           .indexOf(newFilter.toLowerCase()) !== -1
       );
     });
+    //https://stackoverflow.com/questions/51846915/how-do-i-get-the-id-of-a-button-that-was-clicked-reactjs
+    const onShowBtnClick = function(event) {
+      const target = event.currentTarget.id;
+      setFilter(target);
+    };
 
     if (found.length > 10) {
       return (
@@ -40,7 +74,16 @@ const CountryFiltered = function({ countries, newFilter }) {
       );
     } else if (found.length < 10 && found.length > 1) {
       const countriesMapped = found.map(function(country, i) {
-        return <li key={i}>{country.name}</li>;
+        return (
+          <>
+            <li key={i}>
+              {country.name}{" "}
+              <button id={country.name} onClick={onShowBtnClick}>
+                show
+              </button>
+            </li>
+          </>
+        );
       });
       return (
         <div>
@@ -48,6 +91,8 @@ const CountryFiltered = function({ countries, newFilter }) {
         </div>
       );
     } else {
+      console.log("Capital", found[0].capital);
+      setCapital(found[0].capital);
       return (
         <>
           <ShowSingleCountry props={found[0]} />
@@ -76,9 +121,10 @@ const ShowSingleCountry = function({ props }) {
 };
 
 function App() {
-  let ready = false;
   const [newCountry, setCountry] = useState([]);
+  const [capital, setCapital] = useState("");
   const [newFilter, setFilter] = useState("");
+  const [weather, setWeather] = useState({});
 
   useEffect(() => {
     console.log("effect");
@@ -86,18 +132,43 @@ function App() {
     const eventHandler = response => {
       console.log("promise fulfilled", response.data);
       setCountry(response.data);
-      ready = true;
     };
     const promise = axios.get("https://restcountries.eu/rest/v2/all");
     promise.then(eventHandler);
   }, []);
 
-  if (!ready) {
-  }
+  useEffect(() => {
+    console.log("weather effect called!", capital);
+    console.log(
+      "http://api.weatherstack.com/current?access_key=" +
+        api +
+        "&query=" +
+        capital
+    );
+
+    const eventHandler = response => {
+      console.log("promise fulfilled", response.data);
+      setWeather(response.data);
+    };
+    const promise = axios.get(
+      "http://api.weatherstack.com/current?access_key=" +
+        api +
+        "&query=" +
+        capital
+    );
+    promise.then(eventHandler);
+  }, [capital]);
+
   return (
     <>
       <FilterForm setFilter={setFilter} newFilter={newFilter} />
-      <CountryFiltered countries={newCountry} newFilter={newFilter} />
+      <CountryFiltered
+        countries={newCountry}
+        newFilter={newFilter}
+        setFilter={setFilter}
+        setCapital={setCapital}
+      />
+      <GetWeather capital={capital} weather={weather} />
     </>
   );
 }
